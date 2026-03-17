aba68cb63cf8b5adec27df49dadbaa051e1a12c7
RajPatel <raj.patel@pongstudios.com>
2026-03-17 10:19:28 -0400

    GEConfig docekrfile and deploy script

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM alpine:latest
+
+# Install rsync
+RUN apk add --no-cache rsync
+
+WORKDIR /config
+
+# Create the directories
+RUN mkdir -p /config/Internal 
+
+# Copy your data
+COPY ./Internal/ /config/Internal/
---
+++ deploy.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enableDelayedExpansion
+
+REM ===== CONFIG =====
+set VERSION=v9.0.0
+
+REM MUST be lowercase
+set GHCR_REG=ghcr.io
+set GHCR_OWNER=rajp1011
+set GHCR_REPO=pfhbe
+set GHCR_PATH=%GHCR_REG%/%GHCR_OWNER%/%GHCR_REPO%
+
+set CONFIG_IMAGE=%GHCR_PATH%/geconfig:%VERSION%
+
+echo =========================================
+echo == BUILD + PUSH to GHCR
+echo =========================================
+
+echo [1/2] Build...
+
+docker build -t %CONFIG_IMAGE% -f Dockerfile .
+if errorlevel 1 exit /b 1
+
+echo [2/2] Push...
+
+docker push %CONFIG_IMAGE%
+if errorlevel 1 exit /b 1
+
+echo DONE. Pushed GE Config tag: %VERSION%
